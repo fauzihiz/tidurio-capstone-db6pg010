caf26b31146b964e922b2f6f9884ebc672bf7a46
Fauzi Hizbullah <fauzihiz@gmail.com>
2025-07-18 16:46:19 +0700

    add 5 database schema migration, add gitignore, add npm start migrate on packge.json

---
+++ backend/migrations/1752808179708_create-sleep-logs-table.js
@@ -0,0 +1,50 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable('sleep_logs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    userId: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'users(id)',
+      onDelete: 'cascade',
+    },
+    sleepTime: {
+      type: 'TIMESTAMP',
+      notNull: true,
+    },
+    wakeTime: {
+      type: 'TIMESTAMP',
+      notNull: true,
+    },
+    duration: {
+      type: 'INTEGER', // dalam menit
+      notNull: true,
+    },
+    createdAt: {
+      type: 'TIMESTAMP',
+      notNull: true,
+      default: pgm.func('CURRENT_TIMESTAMP'),
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable('sleep_logs');
+};
